f6977193857673b2564e54f9cf8bb26d3dee00de
Romain <Romain@DESKTOP-I5QVIVP>
2022-09-19 16:03:29 +0200

    Ajoutez des fichiers projet.

---
+++ testConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("premier test");
+            Console.ReadLine();
+        }
+    }
+}
